ca7fc756f90648b4bf963387726fb62c5aa9d2fa
ACatSmiling <1172042509@qq.com>
2024-06-26 22:52:14 +0800

    platform self study redis

---
+++ platform-self-study/platform-self-study-redis/src/main/java/cn/zero/cloud/platform/SelfStudyRedisApplication.java
@@ -0,0 +1,15 @@
+package cn.zero.cloud.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XiSun
+ * @since 2024/6/14 23:31
+ */
+@SpringBootApplication
+public class SelfStudyRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SelfStudyRedisApplication.class, args);
+    }
+}
