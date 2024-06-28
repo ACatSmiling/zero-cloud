31a39ab31d3c7c2d462fa94a87cdf53340d45878
ACatSmiling <1172042509@qq.com>
2024-06-28 22:09:01 +0800

    zero cloud self study redis

---
+++ zeloud-self-studies/zeloud-self-study-redis/src/main/java/cn/zero/cloud/platform/RedisApplication.java
@@ -0,0 +1,15 @@
+package cn.zero.cloud.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xisun Wang
+ * @since 2024/6/14 23:31
+ */
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
---
+++ zeloud-self-studies/zeloud-self-study-redis/src/main/java/cn/zero/cloud/platform/demo/JedisDemo.java
@@ -0,0 +1,22 @@
+package cn.zero.cloud.platform.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Xisun Wang
+ * @since 2024/6/14 23:05
+ */
+@Slf4j
+public class JedisDemo {
+    public static void main(String[] args) {
+        /*Jedis jedis = new Jedis("192.168.1.20", 6379);
+
+        jedis.auth("123456");
+
+        log.info("redis connection status: {}", "success");
+        log.info("redis ping value: {}", jedis.ping());
+
+        jedis.set("k1", "jedis");
+        log.info("k1 value: {}", jedis.get("k1"));*/
+    }
+}
