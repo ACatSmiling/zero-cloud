29c2da9d30b9a657bb289b2803e5ef6b7555451e
ACatSmiling <1172042509@qq.com>
2024-06-28 21:53:45 +0800

    zero cloud self study redis distributed lock 1

---
+++ zeloud-self-study/zeloud-self-study-redis-distributed-lock-1/src/main/java/cn/zero/cloud/platform/RedisDistributedLockApplication.java
@@ -0,0 +1,15 @@
+package cn.zero.cloud.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xisun Wang
+ * @since 2024/6/21 22:17
+ */
+@SpringBootApplication
+public class RedisDistributedLockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDistributedLockApplication.class, args);
+    }
+}
---
+++ zeloud-self-study/zeloud-self-study-redis-distributed-lock-1/src/main/java/cn/zero/cloud/platform/service/InventoryService.java
@@ -0,0 +1,9 @@
+package cn.zero.cloud.platform.service;
+
+/**
+ * @author Xisun Wang
+ * @since 2024/6/21 22:21
+ */
+public interface InventoryService {
+    String sale();
+}
