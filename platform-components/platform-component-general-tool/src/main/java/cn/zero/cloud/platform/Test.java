6df876f72a064179e07e9a14c05faa5d9f1777a3
ACatSmiling <1172042509@qq.com>
2024-06-28 20:47:02 +0800

    init zero cloud

--- platform-components/platform-component-general-tool/src/main/java/cn/zero/cloud/platform/Test.java
+++
@@ -1,8 +0,0 @@
-package cn.zero.cloud.platform;
-
-/**
- * @author Xisun Wang
- * @since 6/26/2024 13:36
- */
-public class Test {
-}
--- platform-self-study/platform-self-study-redis-distributed-lock-1/src/main/java/cn/zero/cloud/platform/service/InventoryService.java
+++
@@ -1,9 +0,0 @@
-package cn.zero.cloud.platform.service;
-
-/**
- * @author Xisun Wang
- * @since 2024/6/21 22:21
- */
-public interface InventoryService {
-    String sale();
-}
