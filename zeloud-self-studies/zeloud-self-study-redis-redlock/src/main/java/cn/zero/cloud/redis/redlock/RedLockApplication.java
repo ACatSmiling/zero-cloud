c7030a3c7cff9bcfc40ec429886677f4134d2b45
ACatSmiling <1172042509@qq.com>
2024-06-30 23:31:54 +0800

    zero cloud self study redis redlock

---
+++ zeloud-self-studies/zeloud-self-study-redis-redlock/src/main/java/cn/zero/cloud/redis/redlock/RedLockApplication.java
@@ -0,0 +1,15 @@
+package cn.zero.cloud.redis.redlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xisun Wang
+ * @since 2024/6/30 23:19
+ */
+@SpringBootApplication
+public class RedLockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedLockApplication.class, args);
+    }
+}
