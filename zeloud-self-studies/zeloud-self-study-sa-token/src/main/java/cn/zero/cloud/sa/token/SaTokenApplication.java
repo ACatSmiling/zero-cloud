8842d9d6671b7373b9eccd42e3b21264306d9d6b
ACatSmiling <1172042509@qq.com>
2024-07-02 23:58:55 +0800

    zero cloud self study sa token

---
+++ zeloud-self-studies/zeloud-self-study-sa-token/src/main/java/cn/zero/cloud/sa/token/SaTokenApplication.java
@@ -0,0 +1,15 @@
+package cn.zero.cloud.sa.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xisun Wang
+ * @since 2024/7/2 23:17
+ */
+@SpringBootApplication
+public class SaTokenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenApplication.class, args);
+    }
+}
